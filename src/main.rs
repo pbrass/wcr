c16778d0b7e777d654836caf801cab0c23c0142f
Phil Brass <pbrass@gmail.com>
2025-06-11 22:46:34 -0400

    Added argument processing, tests, now passes all tests.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use clap::Parser;
 use anyhow::Result;
+use clap::Parser;
 use std::fs::File;
+use std::fs;
 use std::io::{self, BufRead, BufReader};
 use unicode_segmentation::UnicodeSegmentation;
 
@@ -9,19 +10,19 @@ use unicode_segmentation::UnicodeSegmentation;
 /// Rust version of `wc`
 struct Args {
     /// List of files
-    #[arg(value_name = "FILE", default_value="-")]
+    #[arg(value_name = "FILE", default_value = "-")]
     files: Vec<String>,
 
     /// print the byte counts
-    #[arg(short('c'), long("bytes"))]
+    #[arg(short('c'), long("bytes"), conflicts_with("print_chars"), conflicts_with("print_graphemes"))]
     print_bytes: bool,
 
     /// print the character counts
-    #[arg(short('m'), long("chars"))]
+    #[arg(short('m'), long("chars"), conflicts_with("print_bytes"), conflicts_with("print_graphemes"))]
     print_chars: bool,
 
     /// print the grapheme counts
-    #[arg(short('g'), long("graphemes"))]
+    #[arg(short('g'), long("graphemes"), conflicts_with("print_chars"), conflicts_with("print_bytes"))]
     print_graphemes: bool,
 
     /// print the newline counts
@@ -31,7 +32,6 @@ struct Args {
     /// print the word counts
     #[arg(short('w'), long("words"))]
     print_words: bool,
-
 }
 
 fn open(filename: &str) -> Result<Box<dyn BufRead>> {
@@ -42,6 +42,7 @@ fn open(filename: &str) -> Result<Box<dyn BufRead>> {
 }
 
 //does wc count line terminators in its character counts?
+//it does in byte-count mode
 //how can we tell the number of *characters* in a string
 
 fn run(_args: Args) -> Result<()> {
@@ -50,46 +51,97 @@ fn run(_args: Args) -> Result<()> {
     let mut totalgraphemes: usize = 0;
     let mut totallines: usize = 0;
     let mut totalwords: usize = 0;
+    let mut pbytes: bool = _args.print_bytes;
+    let pgraphemes: bool = _args.print_graphemes;
+    let mut pwords: bool = _args.print_words;
+    let mut plines: bool = _args.print_lines;
+    let pchars: bool = _args.print_chars;
+    // if there are no flags, then we want lines, words and bytes
+    if !(_args.print_bytes || _args.print_graphemes || _args.print_chars || _args.print_words || _args.print_lines)
+    {
+        pbytes = true;
+        plines = true;
+        pwords = true;
+    }
+    let filecount = _args.files.len();
     for filename in _args.files {
+        //stats for the current file
         let mut filebytes: usize = 0;
         let mut filechars: usize = 0;
         let mut filegraphemes: usize = 0;
         let mut filewords: usize = 0;
         let mut filelines: usize = 0;
+        
         match open(&filename) {
             Err(err) => eprintln!("Failed to open {filename}: {err}"),
-            Ok(h_file) =>
-                {
-                    for line in h_file.lines() {
-                        match line {
-                            Ok(ln) => {
-                                let linebytes = ln.len();
-                                filebytes += linebytes;
-                                totalbytes += linebytes;
-                                let linechars = ln.chars().count();
-                                filechars += linechars;
-                                totalchars += linechars;
-                                let linegraphemes = ln.graphemes(true).count();
-                                filegraphemes += linegraphemes;
-                                totalgraphemes += linegraphemes;
-                                //let linewords: usize = ln.split_word_bounds().count();
-                                let linewords : usize = ln.split_whitespace().count();
-                                filewords += linewords;
-                                totalwords += linewords;
-                                filelines += 1;
-                                totallines += 1;
-                            },
-                            _ => ()
+            Ok(h_file) => {
+                for line in h_file.lines() {
+                    match line {
+                        Ok(ln) => {
+                            let linebytes = ln.len();
+                            filebytes += linebytes;
+                            // for some reason we need to add two characters to the bytecount for 
+                            // each line, if we are reading from stdin, to get the same result
+                            // as GNU wc
+                            if filename == "-" {filebytes += 2}
+                            
+                            // for some reason, ending newlines are part of the "character" count
+                            // in GNU wc? So we have to add one to the character count
+                            let linechars = ln.chars().count() + 1; 
+                            filechars += linechars;
+                            totalchars += linechars;
+                            
+                            // I'm not going to count the newline as a grapheme. Do you think
+                            // I should? Doesn't seem like one to me.
+                            let linegraphemes = ln.graphemes(true).count();
+                            filegraphemes += linegraphemes;
+                            totalgraphemes += linegraphemes;
+                            
+                            // The truly unicode way to count words would be to split on word bounds
+                            // but this includes basically all non-alpha characters, and what
+                            // we mostly mean by words is just whitespace-delimited, so we won't
+                            // use the unicode "split_word_bounds" for now, but I leave it in commented
+                            // as something to think about, maybe an option in the future.
+                            //let linewords: usize = ln.split_word_bounds().count();
+                            let linewords: usize = ln.split_whitespace().count();
+                            filewords += linewords;
+                            totalwords += linewords;
+                            
+                            
+                            filelines += 1;
+                            totallines += 1;
                         }
+                        _ => (/* Should we do something here? Log an error?*/),
                     }
-                    print!("{filelines:>7} {filewords:>7} {filebytes:>7} {filename}\n");
                 }
+                
+                // so if we're not reading from stdin, rather than using the number of bytes
+                // in each line, we can just get the file metadata 
+                if filename != "-" {
+                    let fname = filename.clone(); // thanks, borrow-checker
+                    filebytes = fs::metadata(fname)?.len() as usize;
+                    totalbytes += filebytes;
+                }
+                if plines {print!("{filelines:>8}")}
+                if pwords {print!("{filewords:>8}")}
+                if pbytes {print!("{filebytes:>8}")}
+                if pgraphemes {print!("{filegraphemes:>8}")}
+                if pchars {print!("{filechars:>8}")}
+                if filename != "-" {print!(" {filename}")}
+                print!("\n");
+            }
         }
     }
-    print!("{totallines:>7} {totalwords:>7} {totalbytes:>7} total\n");
+    if filecount > 1 {
+        if plines {print!("{totallines:>8}")}
+        if pwords {print!("{totalwords:>8}")}
+        if pbytes {print!("{totalbytes:>8}")}
+        if pgraphemes {print!("{totalgraphemes:>8}")}
+        if pchars {print!("{totalchars:>8}")}
+        print!(" total\n");
+    }
     Ok(())
 }
-
 
 fn main() {
     if let Err(e) = run(Args::parse()) {
