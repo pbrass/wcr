56bae9485106bf4862adc570a2374684d85e1723
Phil Brass <pbrass@gmail.com>
2025-06-13 16:50:15 -0400

    Moved the count-printing logic into its own function, so per-file and total counts call the same code.

--- src/main.rs
+++ src/main.rs
@@ -81,6 +81,16 @@ fn count(mut file: impl BufRead) -> Result<FileCounts> {
     Ok(count)
 }
 
+fn printcounts(_args: &Args, fcounts: FileCounts, filename: String)  {
+    if _args.print_lines {print!("{:>8}",fcounts.lines)}
+    if _args.print_words {print!("{:>8}",fcounts.words)}
+    if _args.print_bytes {print!("{:>8}",fcounts.bytes)}
+    if _args.print_graphemes {print!("{:>8}",fcounts.graphemes)}
+    if _args.print_chars {print!("{:>8}",fcounts.chars)}
+    if filename != "-" {print!(" {filename}")}
+    print!("\n");
+}
+
 // run the program against the arguments - loop through the filenames, open the files, and count them
 fn run(mut _args: Args) -> Result<()> {
     let mut totals = FileCounts{bytes: 0, chars: 0, graphemes: 0, words: 0, lines: 0 };
@@ -93,7 +103,8 @@ fn run(mut _args: Args) -> Result<()> {
         _args.print_words = true;
     }
     let filecount = _args.files.len();
-    for filename in _args.files {
+    let names = _args.files.clone();
+    for filename in names {
         //stats for the current file
 
         match open(&filename) {
@@ -101,67 +112,11 @@ fn run(mut _args: Args) -> Result<()> {
             Ok(h_file) => {
                 let fcounts = count(h_file)?;
                 totals = totals + fcounts;
-                /*
-                for line in h_file.lines() {
-                    match line {
-                        Ok(ln) => {
-                            let linebytes = ln.len();
-                            filebytes += linebytes;
-                            // for some reason we need to add two characters to the bytecount for 
-                            // each line, if we are reading from stdin, to get the same result
-                            // as GNU wc
-                            if filename == "-" {filebytes += 2}
-                            
-                            // for some reason, ending newlines are part of the "character" count
-                            // in GNU wc? So we have to add one to the character count
-                            let linechars = ln.chars().count() + 1; 
-                            filechars += linechars;
-                            totalchars += linechars;
-                            
-                            // I'm not going to count the newline as a grapheme. Do you think
-                            // I should? Doesn't seem like one to me.
-                            let linegraphemes = ln.graphemes(true).count();
-                            filegraphemes += linegraphemes;
-                            totalgraphemes += linegraphemes;
-                            
-                            // The truly unicode way to count words would be to split on word bounds
-                            // but this includes basically all non-alpha characters, and what
-                            // we mostly mean by words is just whitespace-delimited, so we won't
-                            // use the unicode "split_word_bounds" for now, but I leave it in commented
-                            // as something to think about, maybe an option in the future.
-                            //let linewords: usize = ln.split_word_bounds().count();
-                            let linewords: usize = ln.split_whitespace().count();
-                            filewords += linewords;
-                            totalwords += linewords;
-                            
-                            
-                            filelines += 1;
-                            totallines += 1;
-                        }
-                        _ => (/* Should we do something here? Log an error?*/),
-                    }
-                }
-                
-                 */
-                
-                if _args.print_lines {print!("{:>8}",fcounts.lines)}
-                if _args.print_words {print!("{:>8}",fcounts.words)}
-                if _args.print_bytes {print!("{:>8}",fcounts.bytes)}
-                if _args.print_graphemes {print!("{:>8}",fcounts.graphemes)}
-                if _args.print_chars {print!("{:>8}",fcounts.chars)}
-                if filename != "-" {print!(" {filename}")}
-                print!("\n");
+                printcounts(&_args, fcounts, filename);
             }
         }
     }
-    if filecount > 1 {
-        if _args.print_lines {print!("{:>8}",totals.lines)}
-        if _args.print_words {print!("{:>8}",totals.words)}
-        if _args.print_bytes {print!("{:>8}",totals.bytes)}
-        if _args.print_graphemes {print!("{:>8}",totals.graphemes)}
-        if _args.print_chars {print!("{:>8}",totals.chars)}
-        print!(" total\n");
-    }
+    if filecount > 1 { printcounts(&_args, totals, String::from("total")); }
     Ok(())
 }
 
